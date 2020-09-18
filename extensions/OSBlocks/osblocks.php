369452a8fa670f657497675375fdfb122253e82e
Naleksuh <naleksuh@gmail.com>
2020-09-18 14:40:44 -0700

    Update osblocks.php

--- extensions/OSBlocks/osblocks.php
+++ extensions/OSBlocks/osblocks.php
@@ -3,7 +3,7 @@ class OSBlocks {
   public static function onuserCan( $title, $user, $action, &$result ) {
     if ($title == "Special:BlockList" && !$user->isAllowed('block')) {
       return false;
-		}
+    }
     return true;
   }
 }
