0549bd99649ae45a388aad6b6a2ce8ffcb770428
Naleksuh <naleksuh@gmail.com>
2020-09-18 14:38:32 -0700

    Create osblocks.php

---
+++ extensions/OSBlocks/osblocks.php
@@ -0,0 +1,9 @@
+<?php
+class OSBlocks {
+  public static function onuserCan( $title, $user, $action, &$result ) {
+    if ($title == "Special:BlockList" && !$user->isAllowed('block')) {
+      return false;
+		}
+    return true;
+  }
+}
